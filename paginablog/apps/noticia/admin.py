921421e94532deb3a5a476f42f4039212eda5d3c
farkdev <frk.finanzas.arg@gmail.com>
2022-12-07 19:10:41 -0300

    arreglando

--- paginablog/apps/noticia/admin.py
+++ paginablog/apps/noticia/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Noticia, Categoría
 
 # Register your models here.
+
+@admin.register(Noticia)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'id', 'activo', 'fecha', 'categoria')
+
+
+admin.site.register(Categoría)
